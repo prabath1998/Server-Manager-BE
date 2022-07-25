ca139abd1f8d351b77c89a5da4ab05a132ebf875
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-25 15:55:11 +0530

    add configurations

--- src/main/java/com/test/server/ServerApplication.java
+++ src/main/java/com/test/server/ServerApplication.java
@@ -1,13 +1,26 @@
 package com.test.server;
 
+import com.test.server.enumeration.Status;
+import com.test.server.model.Server;
+import com.test.server.repository.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner run(ServerRepository serverRepository) {
+       return args -> {
+           serverRepository.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+           serverRepository.save(new Server(null, "192.168.2.160", "Fedora Linux", "16 GB", "Personal PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+       };
+    }
 
 }
